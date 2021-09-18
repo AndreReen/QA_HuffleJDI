9355ba0dde9cd8c9f11956ec425eb7ca618223ae
Alexey Nadenenko1 <alexey_nadenenko1@epam.com>
2021-09-18 18:55:45 +0300

    fix driver init

--- src/main/java/webdriver/DriverManager.java
+++ src/main/java/webdriver/DriverManager.java
@@ -1,18 +1,31 @@
 package webdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
 
-    public static WebDriver driver;
+    public static final WebDriver driver;
+    private static final String HEADLESS = "HEADLESS_BROWSER";
+
+    private DriverManager() {
+    }
 
     public static WebDriver setupDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments(
+                "--no-sandbox",
+                "--ignore-certificate-errors");
+            if (Boolean.parseBoolean(System.getProperty(HEADLESS))) {
+                chromeOptions.addArguments("--disable-gpu", "--window-size=1920x1080", "--headless");
+            }
+            driver = new ChromeDriver(chromeOptions);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
         return driver;
