b4f505749ea02233b63f9435979f7bd5b99cdc64
AndreReen <43380198+AndreReen@users.noreply.github.com>
2021-09-18 22:40:38 +0300

    Debugging for jenkins

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources"},
-        glue = {"steps", "hooks"},
+        glue = {"steps", "hooks"}
 )
 public class CucumberRunnerTest {
 }
